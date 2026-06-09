ebae3eead7948e8ac2eec0e1abd40288ee884b2c
Morgan McKenzie <rmtmckenzie@gmail.com>
2026-06-09 15:43:39 +0100

    Change to using hook instead of ffigen

--- hook/build.dart
+++ hook/build.dart
@@ -2,10 +2,6 @@ import 'package:native_toolchain_c/native_toolchain_c.dart';
 import 'package:logging/logging.dart';
 import 'package:hooks/hooks.dart';
 
-// import 'dart:io';
-// import 'package:data_assets/data_assets.dart';
-// import 'build_wasm.dart';
-
 void main(List<String> args) async {
   await build(args, (input, output) async {
     final packageName = input.packageName;
@@ -21,21 +17,5 @@ void main(List<String> args) async {
         ..level = .ALL
         ..onRecord.listen((record) => print(record.message)),
     );
-
-    // if (input.config.buildDataAssets) {
-    //   final packageName = input.packageName;
-      
-    //   await buildWasm('build/out/monocypher.wasm')
-      
-    //   final assetPathInPackage = input.packageRoot.resolve('build/out/monocypher.wasm');
- 
-    //   output.assets.data.add(
-    //     DataAsset(
-    //       package: packageName,
-    //       name: 'monocypher.wasm',
-    //       file: assetPathInPackage,
-    //     ),
-    //   );
-    // }
   });
 }
